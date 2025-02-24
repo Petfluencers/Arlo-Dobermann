56e7fd12da06679e4bc44e0e19f0ffebed356fcb
Melanie Brown <mbrown9702@gmail.com>
2025-02-24 13:17:57 -0600

    updating animations

---
+++ js/body-fade-in.js
@@ -0,0 +1,5 @@
+document.addEventListener("DOMContentLoaded", () => {
+    setTimeout(() => {
+        document.body.style.opacity = "1";
+    }, 100); // Short delay ensures it doesn’t disappear
+});
